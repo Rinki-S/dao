3666eddc334bd90ab38dd63e5545d855dfbbf86a
Rinki-S <iam@rinki.top>
2026-05-24 13:13:33 +0800

    refactor: replace command palette with shadcn command

--- apps/web/src/test/setup.js
+++ apps/web/src/test/setup.js
@@ -2,3 +2,13 @@ import '@testing-library/jest-dom/vitest';
 import { vi } from 'vitest';
 
 Element.prototype.scrollIntoView = vi.fn();
+
+class ResizeObserverMock {
+  observe() {}
+
+  unobserve() {}
+
+  disconnect() {}
+}
+
+globalThis.ResizeObserver = ResizeObserverMock;
